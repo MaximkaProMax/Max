04755239174b3535706dc46d6dfb645683065675
MaximkaProMax <Zholdashev.Maks@yandex.ru>
2024-12-16 21:08:38 +0300

    Max

---
+++ Max/Models/Shift.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Max.Models;
+
+public partial class Shift
+{
+    public int Id { get; set; }
+
+    public int? RoomId { get; set; }
+
+    public DateOnly? Date { get; set; }
+
+    public string? Cleaner { get; set; }
+}
